73983fc8ac9c29bb2c3346eee59d59c6cb178a70
gunha0405 <dhrjsgk0405@gmail.com>
2025-02-11 11:53:13 +0900

    [Feat] User 단일 회원 상세 정보 조회 기능 완성

--- src/main/java/com/example/idus_exam/user/UserController.java
+++ src/main/java/com/example/idus_exam/user/UserController.java
@@ -1,11 +1,14 @@
 package com.example.idus_exam.user;
 
+import com.example.idus_exam.user.model.User;
 import com.example.idus_exam.user.model.UserDto;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -32,6 +35,15 @@ public class UserController {
 
         response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
         return ResponseEntity.ok("로그아웃 성공");
+    }
+
+    @GetMapping("/userInfo")
+    public ResponseEntity<UserDto.UserInfoResponse> userInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        Long userIdx = user.getIdx();
+        UserDto.UserInfoResponse response = userService.getUserInfo(userIdx);
+        return ResponseEntity.ok(response);
     }
 
 }
