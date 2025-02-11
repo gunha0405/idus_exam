64785d81837528af2ee26d2d937d584c7e1e9f79
gunha0405 <dhrjsgk0405@gmail.com>
2025-02-11 11:31:35 +0900

    [Feat] User 로그아웃 기능 완성

--- src/main/java/com/example/idus_exam/user/UserController.java
+++ src/main/java/com/example/idus_exam/user/UserController.java
@@ -1,7 +1,10 @@
 package com.example.idus_exam.user;
 
 import com.example.idus_exam.user.model.UserDto;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +19,19 @@ public class UserController {
     public ResponseEntity<UserDto.SignupResponse> signup(@RequestBody UserDto.SignupRequest request) {
         UserDto.SignupResponse response = userService.signup(request);
         return ResponseEntity.ok(response);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<String> logout(HttpServletResponse response) {
+        ResponseCookie cookie = ResponseCookie.from("ATOKEN", "")
+                .path("/")
+                .httpOnly(true)
+                .secure(true)
+                .maxAge(0) // 즉시 만료
+                .build();
+
+        response.setHeader(HttpHeaders.SET_COOKIE, cookie.toString());
+        return ResponseEntity.ok("로그아웃 성공");
     }
 
 }
